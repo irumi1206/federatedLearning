fixed dataset size weighting

centralserver.py
@@ -20,12 +20,11 @@ class CentralServer:
     def central_train(self):
 
         timepast = 0
-        datasize = 0
 
         # validate the model before training and log it to overall training log file
         logging.info(f"\n{'-'*100}\n")
         accuracyinitial, lossinitial, accuracyperlabelinitial = validate_model_detailed(self.model, self.args.testdataloader, self.args)
-        logging.info(f"Central server round 1, loss : {lossinitial:.2f}, accuracy :{(100*accuracyinitial):.2f}%")
+        logging.info(f"Central server round 0, loss : {lossinitial:.2f}, accuracy :{(100*accuracyinitial):.2f}%")
         logging.info(f"{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelinitial.items()]}")
         self.args.centralservertimepast.append(timepast)
         self.args.centralserverround.append(0)
@@ -57,7 +56,6 @@ class CentralServer:
                     datasizecached[clusterind] = d
                     logging.info(f"{' '*42}{cluster.communicationtime}msec<-")
                 timepast +=epochtimepast
-                datasize += epochdatasize
 
                 # aggregate the models
                 modelstatedict = self.model.state_dict()
@@ -97,12 +95,11 @@ class CentralServer:
             clustereventqueue = PriorityQueue()
             for ind in participatingclusterind:
                 cluster = self.clusterlist[ind]
-                #logging.info(f"{' '*42}->{cluster.communicationtime}msec")
                 loggingcached[ind].append(f"{' '*42}->{cluster.communicationtime}msec")
                 cluster.model.load_state_dict(self.model.state_dict())
                 modelversionforcluster[ind] = modelversion
                 messagequeue = Queue()
-                t, d = cluster.cluster_train(0, messagequeue)
+                t, _ = cluster.cluster_train(0, messagequeue)
                 while not messagequeue.empty():
                     loggingcached[ind].append(messagequeue.get())
                 clustereventqueue.put((t,ind))
@@ -110,7 +107,7 @@ class CentralServer:
             round = 0
 
             for epoch in range(self.centralserverepoch):
-                for _ in range(participatingclusternum):
+                for i in range(participatingclusternum):
                     # get the cluster that fast arrived
                     arrivedtime, arrivedclusterind = clustereventqueue.get()
                     timepast = arrivedtime
@@ -131,7 +128,6 @@ class CentralServer:
                         logging.info(f"{' '*42}{cluster.communicationtime}msec<-")
                         modelstatedict = self.model.state_dict()
                         for key in modelstatedict:
-                            #modelstatedict[key] = torch.zeros_like(modelstatedict[key])
                             modelstatedict[key] = alphatime * cluster.model.state_dict()[key] + (1-alphatime) * self.model.state_dict()[key]
                         self.model.load_state_dict(modelstatedict)
                         modelversion +=1
@@ -145,7 +141,7 @@ class CentralServer:
                     self.clusterlist[pickedclusterind].model.load_state_dict(self.model.state_dict())
                     modelversionforcluster[pickedclusterind] = modelversion
                     messagequeue = Queue()
-                    t,d = self.clusterlist[pickedclusterind].cluster_train(modelversion,messagequeue)
+                    t,_ = self.clusterlist[pickedclusterind].cluster_train(modelversion,messagequeue)
                     loggingcached[pickedclusterind].clear()
                     loggingcached[pickedclusterind].append(f"{' '*42}->{self.clusterlist[pickedclusterind].communicationtime}msec")
                     while not messagequeue.empty():
@@ -160,9 +156,9 @@ class CentralServer:
                     
                     round +=1
 
-                self.args.centralservertimepast.append(timepast)
-                self.args.centralserverround.append(epoch+1)
-                self.args.centralserveraccuracy.append(accuracyafter)
-                self.args.centralserverloss.append(lossafter)   
+                    self.args.centralservertimepast.append(timepast)
+                    self.args.centralserverround.append(1+epoch+i/participatingclusternum)
+                    self.args.centralserveraccuracy.append(accuracyafter)
+                    self.args.centralserverloss.append(lossafter)   
 
 

client.py
@@ -31,9 +31,6 @@ class Client:
     def local_train(self,queue):
 
         self.model.to(self.args.device)
-        torch.manual_seed(self.args.randomseed)
-        torch.cuda.manual_seed(self.args.randomseed)
-        torch.cuda.manual_seed_all(self.args.randomseed)
 
         # reset optimizer
         self.optimizer = get_optimizer(self.model, self.args.optimizername, self.args.learningrate)
@@ -62,12 +59,11 @@ class Client:
                 self.optimizer.step()
         
         # validate the model after training
-    
         localaccuracyafter, _= validate_model(self.model, self.dataloader, self.args)
         globalaccuracyafter, _= validate_model(self.model, self.args.testdataloader, self.args)
         queue.put(f"{' '*94}<-> Client {self.clientid}, global : from {(100*globalaccuracybefore):.2f}% to {(100*globalaccuracyafter):.2f}%, local : from {(100*localaccuracybefore):.2f}% to {(100*localaccuracyafter):.2f}%, training time : {self.calculate_training_time()}msec")
 
         # calculate the training time
-        trainingtime = self.computationtimeperbatch * self.localepoch * ceil(len(self.dataloader.dataset) / int(self.dataloader.batch_size)) + 2*self.communicationtime
+        trainingtime = self.calculate_training_time()
         datasize = len(self.dataloader.dataset)
         return trainingtime, datasize

cluster.py
@@ -1,14 +1,10 @@
 import torch
 import torch.nn as nn
-import logging
-from utils import validate_model, get_model, validate_model_detailed
+from utils import get_model, validate_model_detailed
 from queue import PriorityQueue
 from queue import Queue
-import numpy as np
 import random
 from collections import defaultdict
-def train_single_client(client,queue):
-    client.local_train(queue)
 
 class Cluster:
     def __init__(self, clusterid, communicationtime, intraclusteringtype, clusterepoch, args, clientlist):
@@ -32,8 +28,6 @@ class Cluster:
 
         # validate the model before training
         accuracybefore, lossbefore, accuracyperlabelbefore = validate_model_detailed(self.model, self.testdataloader, self.args)
-        # logging.info(f"{' '*53}-> Cluster {self.clusterid}, loss : {lossbefore:.2f}, accuracy {(100*accuracybefore):.2f}%, model from round {modelroundnumber+1}")
-        # logging.info(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelbefore.items()]}")
         messagequeue.put(f"{' '*53}-> Cluster {self.clusterid}, loss : {lossbefore:.2f}, accuracy {(100*accuracybefore):.2f}%, model from round {modelroundnumber+1}")
         messagequeue.put(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelbefore.items()]}")
 
@@ -64,7 +58,6 @@ class Cluster:
                 #log the results
                 while not q.empty():
                     messagequeue.put(q.get())
-                    #logging.info(q.get())
 
                 # aggregate the models based on the dataset size
                 modelstatedict = self.model.state_dict()
@@ -79,9 +72,6 @@ class Cluster:
 
                 # validate the model after training and log it to overall training log
                 accuracyafter, lossafter, accuracyperlabelafter = validate_model_detailed(self.model, self.testdataloader, self.args)
-                # logging.info(f"{' '*53}Cluster {self.clusterid}, round {i+1}, loss : {lossafter:.2f}, accuracy {(100*accuracyafter):.2f}% <-")
-                # logging.info(f"{' '*53}Time past : {timepast}msec")
-                # logging.info(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelafter.items()]}")
                 messagequeue.put(f"{' '*53}Cluster {self.clusterid}, round {i+1}, loss : {lossafter:.2f}, accuracy {(100*accuracyafter):.2f}% <-")
                 messagequeue.put(f"{' '*53}Time past : {timepast}msec")
                 messagequeue.put(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelafter.items()]}")
@@ -126,7 +116,6 @@ class Cluster:
                     # get the client that fast arrived
                     arrivedtime, arrivedclientind = clienteventqueue.get()
                     timepast = arrivedtime
-                    datasize += datasizecached[arrivedclientind]
                     client = self.clientlist[arrivedclientind]
                     loggingmessage = loggingcached[arrivedclientind]
                     for message in loggingmessage:
@@ -164,14 +153,13 @@ class Cluster:
 
                     # validate the model after training and log it to overall training log
                     accuracyafter, lossafter, accuracyperlabelafter = validate_model_detailed(self.model, self.testdataloader, self.args)
-                    # logging.info(f"{' '*53}Cluster {self.clusterid}, round {round+1}, accuracy {(100*accuracyafter):.2f}% <-")
-                    # logging.info(f"{' '*53}Time past : {timepast}msec")
-                    # logging.info(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelafter.items()]}")
                     messagequeue.put(f"{' '*53}Cluster {self.clusterid}, round {round+1}, accuracy {(100*accuracyafter):.2f}% <-")
                     messagequeue.put(f"{' '*53}Time past : {timepast}msec")
                     messagequeue.put(f"{' '*53}{[f'{label}:{(accuracy*100):.2f}%' for label, accuracy in accuracyperlabelafter.items()]}")
 
                     round +=1
+
+            datasize += sum(datasizecached.values())
 
             timepast += self.communicationtime
 

models/cnncifar10.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # About 33MB
-class CNNCifar10(nn.Module):
+class CNNCIFAR10(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)   # input: [B, 3, 32, 32]
@@ -23,4 +23,4 @@ class CNNCifar10(nn.Module):
         return x
 
 def get_model():
-    return CNNCifar10()
+    return CNNCIFAR10()

partitiondata.py
@@ -1,11 +1,9 @@
-from torchvision import datasets, transforms
 import numpy as np
-import torch
 from torch.utils.data import DataLoader, Subset
 from collections import defaultdict
-import os
-import random
+
 from dataset.FEMNISTClientDataset import FEMNISTClientDataset
+
 
 # Return list of dataloader splited based on the setting also save data distribution for each client and global data distribution(args.labelpercentageperclient, args.labelpercentageforglobaldistribution)
 def partition_data(args):
@@ -17,21 +15,22 @@ def partition_data(args):
         dataperlabel = defaultdict(int)
         dataperclient = defaultdict(list)
         totalsample = len(args.traindataset)
-
+        
         for sample in args.traindataset:
             dataperlabel[sample["character"]] +=1
             dataperclient[sample["writer_id"]].append(sample)
 
         for label in dataperlabel.keys():
             args.labelpercentageforglobaldistribution[label] = dataperlabel[label]/totalsample
-        print(args.labelpercentageforglobaldistribution)
         
         for samples in dataperclient.values():
             clientdataset = FEMNISTClientDataset(samples)
             clientdataloader = DataLoader(clientdataset, batch_size=args.batchsize, shuffle =True)
             dataloaderlist.append(clientdataloader)
 
-        args.clientnum = len(dataloaderlist)
+
+        if len(dataloaderlist) != args.clientnum:
+            raise ValueError("partitioning error")
 
         return dataloaderlist
             

utils.py
@@ -13,6 +13,7 @@ from datasets import load_dataset
 from random import seed,shuffle
 from dataset.FEMNISTClientDataset import FEMNISTClientDataset
 from torch.utils.data import Subset
+import random
 
 # Return model object based on the model name
 def get_model(modelname):
@@ -21,6 +22,9 @@ def get_model(modelname):
         module = importlib.import_module(f"models.{modelname}")
         return module.get_model()
     elif modelname == "cnncifar10":
+        module = importlib.import_module(f"models.{modelname}")
+        return module.get_model()
+    elif modelname == "cnnfemnist":
         module = importlib.import_module(f"models.{modelname}")
         return module.get_model()
     else :
@@ -36,24 +40,50 @@ def get_dataset(datasetname,args):
         traindataset = datasets.CIFAR10(root = './data', train = True, download = True, transform = transforms.ToTensor() )
         testdataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.ToTensor())
     elif datasetname == "femnist":
+
         # get the dataset
         dataset = load_dataset("flwrlabs/femnist", split = "train")
-        dataset.set_format("torch")
 
-        # shuffle and split
-        seed(args.randomseed)
-        indices = list(range(len(dataset)))
-        shuffle(indices)
+        # get clietnum + testing clients from the dataset
+        writer = dataset.unique("writer_id")
         testratio = 0.2
-        testsize = int(len(indices)*testratio)
-        testindices = indices[:testsize]
-        trainindices = indices[testsize:]
+        clientnumtosample = args.clientnum + int(args.clientnum*testratio)
+        random.seed(args.randomseed)
+        sampledwriter = random.sample(writer,clientnumtosample)
+        testwriter = sampledwriter[args.clientnum:]
+        trainwriter = sampledwriter[:args.clientnum]
+        testwriterset = set(testwriter)
+        trainwriterset = set(trainwriter)
 
-        #dataset = FEMNISTClientDataset(dataset)
-        testdataset = Subset(dataset, testindices)
+        testdataset = dataset.filter(lambda example: example["writer_id"] in testwriterset)
+        traindataset = dataset.filter(lambda example: example["writer_id"] in trainwriterset)
+
+        imagetransform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+
+        testdataset.set_transform(
+            lambda batch: {
+                'image': [imagetransform(img) for img in batch['image']],
+                'writer_id': batch['writer_id'],
+                'character': batch['character']
+                # Add any other columns you want to preserve here
+            }
+        )
+        traindataset.set_transform(
+            lambda batch: {
+                'image': [imagetransform(img) for img in batch['image']],
+                'writer_id': batch['writer_id'],
+                'character': batch['character']
+                # Add any other columns you want to preserve here
+            }
+        )
+
+        print(len(testdataset))
+        
+        print(len(traindataset))
+
         testdataset = FEMNISTClientDataset(testdataset)
-        traindataset = Subset(dataset, trainindices)
-
     
     else:
         raise ValueError("datasetname not supported")