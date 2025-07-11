02bafa8c72754bbf586c48f3e747e994428cbb4d
Sungha Kim <sungha.kim@gmail.com>
2025-07-11 20:20:28 +0900

    machine 3 prompt

---
+++ runhierfl2.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+python3 main.py \
+        -interclusteringtype async \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbyrandomshufflecustom \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 5 \
+        -localepochtype fixed \
+        -localepoch 5
+
+# python3 main.py \
+#         -interclusteringtype async \
+#         -intraclusteringtype sync \
+#         -modelname cnncifar10 \
+#         -datasetname cifar10 \
+#         -clientnum 100 \
+#         -dataheterogeneitytype onelabeldominant \
+#         -computationcapabilitymatric fixed\
+#         -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+#         -clustercommunicationtime 800 \
+#         -clusteringtype clusterbygradientsimilarity \
+#         -clusternum 10 \
+#         -clustersize 10 \
+#         -centralserverepoch 100 \
+#         -clusterepochtype fixed \
+#         -clusterepoch 5 \
+#         -localepochtype fixed \
+#         -localepoch 5
+
+# python3 main.py \
+#         -interclusteringtype async \
+#         -intraclusteringtype sync \
+#         -modelname cnncifar10 \
+#         -datasetname cifar10 \
+#         -clientnum 100 \
+#         -dataheterogeneitytype onelabeldominant \
+#         -computationcapabilitymatric fixed\
+#         -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+#         -clustercommunicationtime 800 \
+#         -clusteringtype clusterbygradientdisimilarity \
+#         -clusternum 10 \
+#         -clustersize 10 \
+#         -centralserverepoch 100 \
+#         -clusterepochtype fixed \
+#         -clusterepoch 5 \
+#         -localepochtype fixed \
+#         -localepoch 5
+
+
