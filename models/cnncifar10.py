1d954752408b2ba6d53d30de366557cee4266f5a
Sungha Kim <sungha.kim@gmail.com>
2025-07-11 20:03:28 +0900

    fixed model

--- models/cnncifar10.py
+++ models/cnncifar10.py
@@ -5,21 +5,20 @@ import torch.nn.functional as F
 class CNNCIFAR10(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=5)   # input: [B, 3, 32, 32]
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=5)  # -> [B, 64, 32, 32]
-        self.pool = nn.MaxPool2d(2, 2)                            # -> [B, 64, 16, 16]
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=5)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=5)
+        self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(64 * 5 * 5, 384)
-        self.fc2 = nn.Linear(512, 10)
-        self.fc3 = 
+        self.fc2 = nn.Linear(384, 192)
+        self.fc3 = nn.Linear(192, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))      # [B, 32, 32, 32]
-        x = F.relu(self.conv2(x))      # [B, 64, 32, 32]
-        x = self.pool(x)               # [B, 64, 16, 16]
-        x = self.dropout(x)
-        x = x.view(-1, 64 * 16 * 16)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64*5*5)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)                # logits
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 def get_model():
