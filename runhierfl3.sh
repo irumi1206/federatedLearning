05f02e7b5e6533d7f6e4afdbe35a8af995b173b1
mnet <sungha.kim123@gmail.com>
2025-07-12 00:29:46 +0900

    d

--- runhierfl3.sh
+++ runhierfl3.sh
@@ -1,23 +1,18 @@
-#!/usr/bin/env bash
-
 python3 main.py \
         -interclusteringtype sync \
         -intraclusteringtype sync \
-        -modelname cnnmnist \
-        -datasetname mnist \
-        -clientnum 10 \
+        -modelname cnnfemnist \
+        -datasetname femnist \
+        -clientnum 100 \
         -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
         -systemheterogeneity alltimesame \
         -clustercommunicationtime 800 \
         -clusteringtype clusterbyrandomshuffle \
-        -clusternum 5 \
-        -clustersize 2 \
+        -clusternum 10 \
+        -clustersize 10 \
         -centralserverepoch 100 \
         -clusterepochtype fixed \
         -clusterepoch 2 \
         -localepochtype fixed \
         -localepoch 3
-
-
-
