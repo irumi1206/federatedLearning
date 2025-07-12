f31ae876b1ab8e76adc242383cffde1a8cbcb1f0
Sungha Kim <sungha.kim@gmail.com>
2025-07-12 11:42:49 +0900

    random

--- main.py
+++ main.py
@@ -34,17 +34,20 @@ def setting(args):
 
     # fix randomness
     seed = args.randomseed
-    #os.environ["PYTHONHASHSEED"] = str(seed)
-    #os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"  
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"  
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    #torch.backends.cudnn.deterministic = True
-    #torch.backends.cudnn.benchmark = False
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
-    #torch.use_deterministic_algorithms(True)
+
+    torch.use_deterministic_algorithms(True)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
+
     torch.set_float32_matmul_precision("high")
     mp.set_start_method('spawn', force=True)
 
