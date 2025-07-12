f689f7ad1bc722b41ef4923600c4f9c8f6818c8d
Sungha Kim <sungha.kim@gmail.com>
2025-07-12 11:34:16 +0900

    randomness

--- runhierfl1.sh
+++ runhierfl1.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-# python3 main.py \
-#         -interclusteringtype async \
-#         -intraclusteringtype sync \
-#         -modelname cnncifar10 \
-#         -datasetname cifar10 \
-#         -clientnum 100 \
-#         -dataheterogeneitytype dirichletdistribution \
-#         -computationcapabilitymatric fixed\
-#         -systemheterogeneity realistic \
-#         -clustercommunicationtime 800 \
-#         -clusteringtype clusterbygradientsimilarity \
-#         -clusternum 10 \
-#         -clustersize 10 \
-#         -centralserverepoch 100 \
-#         -clusterepochtype fixed \
-#         -clusterepoch 10 \
-#         -localepochtype fixed \
-#         -localepoch 2
+python3 main.py \
+        -interclusteringtype async \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype dirichletdistribution \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity realistic \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientsimilarity \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2
 
 python3 main.py \
         -interclusteringtype async \
