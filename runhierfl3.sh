609a0021f567a35f569a3929bdf173aa8815eb37
Sungha Kim <sungha.kim@gmail.com>
2025-07-12 11:57:24 +0900

    bash

--- runhierfl3.sh
+++ runhierfl3.sh
@@ -5,16 +5,73 @@ python3 main.py \
         -intraclusteringtype sync \
         -modelname cnncifar10 \
         -datasetname cifar10 \
-        -clientnum 10 \
+        -clientnum 100 \
         -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
-        -systemheterogeneity alltimesame \
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
         -clustercommunicationtime 800 \
-        -clusteringtype clusterbyrandomshuffle \
-        -clusternum 5 \
-        -clustersize 2 \
+        -clusteringtype clusterbygradientsimilarity \
+        -clusternum 10 \
+        -clustersize 10 \
         -centralserverepoch 100 \
         -clusterepochtype fixed \
-        -clusterepoch 2 \
+        -clusterepoch 10 \
         -localepochtype fixed \
-        -localepoch 3
+        -localepoch 2
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilarity \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilarityandsystemsimilarity \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbyrandomshufflecustom \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2
