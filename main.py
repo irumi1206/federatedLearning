19836b55a3e18acf0a74e9fd4bb9b85f95db3ece
Sungha Kim <sungha.kim@gmail.com>
2025-07-12 11:11:13 +0900

    randomness

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ def setting(args):
     np.random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.use_deterministic_algorithms(True)
