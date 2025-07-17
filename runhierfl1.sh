f868c20055c12984324f1fa0891e470c2da7df79
mnet <sungha.kim123@gmail.com>
2025-07-18 03:33:55 +0900

    fix run1

--- runhierfl1.sh
+++ runhierfl1.sh
@@ -2,27 +2,27 @@
 
 # bash script for home machine
 
-python3 main.py \
-        -interclusteringtype sync \
-        -intraclusteringtype sync \
-        -modelname cnncifar10 \
-        -datasetname cifar10 \
-        -clientnum 100 \
-        -dataheterogeneitytype onelabeldominant \
-        -computationcapabilitymatric fixed\
-        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
-        -clustercommunicationtime 800 \
-        -clusteringtype clusterbygradientsimilarity \
-        -clusternum 10 \
-        -clustersize 10 \
-        -centralserverepoch 100 \
-        -clusterepochtype fixed \
-        -clusterepoch 5 \
-        -localepochtype fixed \
-        -localepoch 2 \
-        -examinethemodelindetail 0 \
-        -learningrate 0.1 \
-        -learningratedecay 0.992
+# python3 main.py \
+#         -interclusteringtype sync \
+#         -intraclusteringtype sync \
+#         -modelname cnncifar10 \
+#         -datasetname cifar10 \
+#         -clientnum 100 \
+#         -dataheterogeneitytype onelabeldominant \
+#         -computationcapabilitymatric fixed\
+#         -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+#         -clustercommunicationtime 800 \
+#         -clusteringtype clusterbygradientsimilarity \
+#         -clusternum 10 \
+#         -clustersize 10 \
+#         -centralserverepoch 100 \
+#         -clusterepochtype fixed \
+#         -clusterepoch 5 \
+#         -localepochtype fixed \
+#         -localepoch 2 \
+#         -examinethemodelindetail 0 \
+#         -learningrate 0.1 \
+#         -learningratedecay 0.992
 
 python3 main.py \
         -interclusteringtype sync \
@@ -30,7 +30,7 @@ python3 main.py \
         -modelname cnncifar10 \
         -datasetname cifar10 \
         -clientnum 100 \
-        -dataheterogeneitytype dirichletdistribution \
+        -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
         -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
         -clustercommunicationtime 800 \
