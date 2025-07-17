c224a5368d568d1d440c3d5d33ff53ae5b8690fc
mnet <sungha.kim123@gmail.com>
2025-07-18 03:46:05 +0900

    fix

--- runhierfl3.sh
+++ runhierfl3.sh
@@ -1,6 +1,6 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# # bash script for home machine
+# bash script for home machine
 
 python3 main.py \
         -interclusteringtype sync \
@@ -8,18 +8,59 @@ python3 main.py \
         -modelname cnncifar10 \
         -datasetname cifar10 \
         -clientnum 100 \
-        -dataheterogeneitytype onlyspecificlabel \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientsimilarity \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 20 \
+        -clusterepochtype fixed \
+        -clusterepoch 5 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.05 \
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
         -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
         -clustercommunicationtime 800 \
         -clusteringtype clusterbygradientdissimilarity \
         -clusternum 10 \
         -clustersize 10 \
-        -centralserverepoch 100 \
+        -centralserverepoch 20 \
         -clusterepochtype fixed \
         -clusterepoch 5 \
         -localepochtype fixed \
         -localepoch 2 \
         -examinethemodelindetail 0 \
-        -learningrate 0.1 \
-        -learningratedecay 0.992
+        -learningrate 0.05 \
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientbetweenassumeonelabeldominant100clients \
+        -clusternum 10 \
+        -clustersize 10 \
+        -centralserverepoch 20 \
+        -clusterepochtype fixed \
+        -clusterepoch 5 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.05 \
