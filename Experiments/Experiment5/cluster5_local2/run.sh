0fcb1a6e0a0f8a1c0f24f014decfe1326d6bc343
Sungha Kim <sungha.kim@gmail.com>
2025-07-19 15:05:18 +0900

    experiment

--- Experiments/Experiment5/cluster5_local2/run.sh
+++ Experiments/Experiment5/cluster5_local2/run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
-# bash script for lab machine 1
+bash script for lab machine 2
 
 python3 main.py \
         -interclusteringtype sync \
@@ -10,11 +10,10 @@ python3 main.py \
         -clientnum 100 \
         -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
-        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -systemheterogeneity alltimesame \
         -clustercommunicationtime 800 \
         -clusteringtype clusterbygradientsimilarity \
         -clusternum 10 \
-        -clustersize 10 \
         -centralserverepoch 100 \
         -clusterepochtype fixed \
         -clusterepoch 5 \
@@ -32,11 +31,10 @@ python3 main.py \
         -clientnum 100 \
         -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
-        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -systemheterogeneity alltimesame \
         -clustercommunicationtime 800 \
         -clusteringtype clusterbygradientdissimilarity \
         -clusternum 10 \
-        -clustersize 10 \
         -centralserverepoch 100 \
         -clusterepochtype fixed \
         -clusterepoch 5 \
@@ -54,11 +52,31 @@ python3 main.py \
         -clientnum 100 \
         -dataheterogeneitytype onelabeldominant \
         -computationcapabilitymatric fixed\
-        -systemheterogeneity assume100clients95onelabeldominance_foreachlabel100200300400500of2randomlydistributed_comm3 \
+        -systemheterogeneity alltimesame \
         -clustercommunicationtime 800 \
         -clusteringtype clusterbygradientbetweenassumeonelabeldominant100clients \
         -clusternum 10 \
-        -clustersize 10 \
+        -centralserverepoch 100 \
+        -clusterepochtype fixed \
+        -clusterepoch 5 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.1 \
+        -learningratedecay 0.992
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbyrandomshuffle \
+        -clusternum 10 \
         -centralserverepoch 100 \
         -clusterepochtype fixed \
         -clusterepoch 5 \
