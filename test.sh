0cf7eb6e8f5fb1fb24e6c99685584cfb58ca340a
Sungha Kim <sungha.kim@gmail.com>
2025-07-22 03:20:42 +0900

    fix

---
+++ test.sh
@@ -0,0 +1,198 @@
+<<<<<<< HEAD
+=======
+#!/usr/bin/env bash
+
+# bash script for lab machine 2
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype dirichletdistribution \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.5
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype dirichletdistribution \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.1
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype dirichletdistribution \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.05
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype dirichletdistribution \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.01
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 1.0
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.1
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.05
+
+python3 main.py \
+        -interclusteringtype sync \
+        -intraclusteringtype sync \
+        -modelname cnncifar10 \
+        -datasetname cifar10 \
+        -clientnum 100 \
+        -dataheterogeneitytype onelabeldominant \
+        -dirichletalpha 0.01 \
+        -computationcapabilitymatric fixed\
+        -systemheterogeneity alltimesame \
+        -clustercommunicationtime 800 \
+        -clusteringtype clusterbygradientdissimilaritygreedy \
+        -clusternum 10 \
+        -centralserverepoch 200 \
+        -clusterepochtype fixed \
+        -clusterepoch 10 \
+        -localepochtype fixed \
+        -localepoch 2 \
+        -examinethemodelindetail 0 \
+        -learningrate 0.01 \
+        -clusterfrequency 200 \
+        -balancelambda 0.01
+
+
+
+
+
+
+
+
+>>>>>>> a6de74e (fix)
